e7d6cc36c6464a81034de404e99e5c580d2aecf4
gagan0183 <g.deepsingh1@gmail.com>
2020-06-08 14:22:03 +0530

    initial

---
+++ Java8/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 9;
+        int i = 1000;
+        long l = 50000 + 10 * (b + s + i);
+        System.out.print(l);
+    }
+}
