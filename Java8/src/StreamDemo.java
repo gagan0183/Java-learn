1a386dcd0d73c712f7fa7005cbbebb22460fd4ba
gagan0183 <g.deepsingh1@gmail.com>
2020-06-08 16:10:17 +0530

    stream filter example

--- Java8/src/StreamDemo.java
+++ Java8/src/StreamDemo.java
@@ -5,7 +5,9 @@ import java.util.stream.Stream;
 public class StreamDemo {
     public static void main(String[] args) {
         Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        stream.forEach(p -> System.out.println(p));
+        // stream.forEach(p -> System.out.println(p));
+
+        stream.filter(p -> p <= 5).forEach(System.out::println);
 
         List<String> list = new ArrayList<>();
         list.add("a");
