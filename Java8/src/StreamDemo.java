25e891c9ced6313dba8f178f8a3206d76d00ac5b
gagan0183 <g.deepsingh1@gmail.com>
2020-06-08 16:05:42 +0530

    stream list of strings

--- Java8/src/StreamDemo.java
+++ Java8/src/StreamDemo.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main(String[] args) {
         Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
         stream.forEach(p -> System.out.println(p));
+
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("e");
+
+        Stream<String> streamOfStrings = list.stream();
+        streamOfStrings.forEach(p -> System.out.println(p));
     }
 }
