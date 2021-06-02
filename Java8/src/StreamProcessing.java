3acfd4e91f60192252e645a9a6c7db7e18cbf403
gagan0183 <g.deepsingh1@gmail.com>
2021-06-02 16:30:43 +0530

    streams

---
+++ Java8/src/StreamProcessing.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class StreamProcessing {
+    public static void main(String[] args) {
+        List<Student> students = getStudents();
+        System.out.println(students);
+    }
+
+    private static List<Student> getStudents() {
+        Exam javaExam = new Exam("Java");
+        Exam pythonExam = new Exam("Python");
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(91, "Mary", javaExam));
+        students.add(new Student(91, "Annabelle", pythonExam));
+        students.add(new Student(100, "Monty", pythonExam));
+        return students;
+    }
+
+    static class Student {
+        int roundedPercentage;
+        String name;
+        Exam exam;
+
+        public Student(int roundedPercentage, String name, Exam exam) {
+            this.roundedPercentage = roundedPercentage;
+            this.name = name;
+            this.exam = exam;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "roundedPercentage=" + roundedPercentage +
+                    ", name='" + name + '\'' +
+                    ", exam=" + exam +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Student student = (Student) o;
+            return roundedPercentage == student.roundedPercentage && Objects.equals(name, student.name) && Objects.equals(exam, student.exam);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(roundedPercentage, name, exam);
+        }
+    }
+
+    static class Exam {
+        String name;
+
+        public Exam(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Exam{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Exam exam = (Exam) o;
+            return Objects.equals(name, exam.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
