4704e9444284482d46a86ac560fffa680c97ccaa
gagan0183 <g.deepsingh1@gmail.com>
2021-06-02 19:18:38 +0530

    static method reference changes

--- Java8/src/FunctionReference.java
+++ Java8/src/FunctionReference.java
@@ -1,13 +1,20 @@
 import java.io.PrintStream;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class FunctionReference {
     public static void main(String[] args) {
+        //bound instance method reference
         Consumer<String> bound = System.out::println;
         bound.accept("Hello from bound method reference");
 
+        //unbound instance method reference
         BiConsumer<PrintStream, String> unbound = PrintStream::println;
         unbound.accept(System.out,"Hello from unbound method reference");
+
+        //static method reference
+        Supplier<Thread> staticMethodReference = Thread::currentThread;
+        System.out.println("Static method reference " + staticMethodReference.get());
     }
 }
